cf102a97016cff5c3f7e5cba55c08891d78d62b2
David Tiago Conceição <davidtc.developer@gmail.com>
2017-10-11 20:09:39 -0300

    Extracting interceptor to external class

--- app/src/main/java/com/developer/davidtc/flickrpublicfeedandroid/rest/retrofit/RetrofitServiceGenerator.java
+++ app/src/main/java/com/developer/davidtc/flickrpublicfeedandroid/rest/retrofit/RetrofitServiceGenerator.java
@@ -4,14 +4,9 @@ import android.support.annotation.NonNull;
 
 import com.developer.davidtc.flickrpublicfeedandroid.BuildConfig;
 
-import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
-import okhttp3.HttpUrl;
-import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
 import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -56,21 +51,4 @@ public final class RetrofitServiceGenerator {
         return okHttpClient;
     }
 
-    private static class HeaderFormatInterceptor implements Interceptor {
-        @Override
-        public Response intercept(Chain chain) throws IOException {
-            Request original = chain.request();
-            String format = original.header("format");
-            if (format == null) {
-                return chain.proceed(original);
-            }
-            HttpUrl url = original.url()
-                    .newBuilder()
-                    .addQueryParameter("format", format)
-                    .build();
-            return chain.proceed(original.newBuilder()
-                    .url(url)
-                    .build());
-        }
-    }
 }
